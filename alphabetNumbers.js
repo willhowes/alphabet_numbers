07aebbc32d8472b6d51e14d2ddca2de3da8bcdd3
Will Howes <williameldenhowes@gmail.com>
2019-10-11 15:30:31 +0100

    Refactored to use an object with alphabet number references and a helper function to split the array

--- alphabetNumbers.js
+++ alphabetNumbers.js
@@ -1,8 +1,26 @@
-const alphabetNumbers = (letter) => {
-  if(letter === 'a') {
-    return '1'
-  }
-  return '1 2' 
+const alphabetNumberReference = {
+'a': '1', 'b': '2'}
+
+const alphabetNumbers = (string) => {
+
+  const arrayOfString = toArray(string);
+  const finalArray = []
+  arrayOfString.forEach(character => {
+    finalArray.push(alphabetNumberReference[character])
+  })
+
+  // if(string === 'a') {
+  //   return '1'
+  // }
+  // return '1 2'
+
+  return finalArray.join(" ")
 }
 
+const toArray = (stringToConvert) => {
+  return stringToConvert
+    .replace(/[^\w]/g, "")
+    .toLowerCase()
+    .split("")
+}
 module.exports = alphabetNumbers
