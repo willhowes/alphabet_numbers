037c71e956364950ed7848a5180077d343601e90
eldenhowes <williameldenhowes@gmail.com>
2019-10-12 11:27:24 +0100

    The alphabetNumbers function ignores non alphabet characters

--- alphabetNumbers.test.js
+++ alphabetNumbers.test.js
@@ -1,15 +1,19 @@
-const alphabetNumbers = require('./alphabetNumbers')
+const alphabetNumbers = require("./alphabetNumbers");
 
-describe('alphabetNumbers', () => {
+describe("alphabetNumbers", () => {
   it('returns 1 for "a"', () => {
-    expect(alphabetNumbers('a')).toEqual('1')
-  })
+    expect(alphabetNumbers("a")).toEqual("1");
+  });
 
   it('returns "1 2" for an input of "ab"', () => {
-    expect(alphabetNumbers('ab')).toEqual('1 2')
-  })
+    expect(alphabetNumbers("ab")).toEqual("1 2");
+  });
 
   it('returns "2 1 26" for and input of "baz"', () => {
-    expect(alphabetNumbers('baz')).toEqual('2 1 26')
-  })
-})
+    expect(alphabetNumbers("baz")).toEqual("2 1 26");
+  });
+
+  it("ignores non-letters", () => {
+    expect(alphabetNumbers("hi!.,:;`~@#${%^&*()}|[]/")).toEqual("8 9");
+  });
+});
